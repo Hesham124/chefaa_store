865422faa459e3983d140acb1a710fe3e27230f0
Hesham kamal <hesham@chefaa.com>
2019-06-02 14:04:40 +0200

    create custonm css file for only header and footer

--- header.php
+++ header.php
@@ -23,6 +23,9 @@
     <!-- Start Calling Go To Top Button CSS File -->
     <link rel = "stylesheet" href = "css/go_to_top_button.css" />
     <!-- End Calling Go To Top Button Css File  -->
+    <!-- Start Calling Header And Footer CSS Style -->
+    <link rel = "stylesheet" href = "css/header_and_footer.css" />
+    <!-- End Calling Header And Footer CSS Style -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.min.js"></script>
       <script src = 'js/respond.min.js' ></script>
