b06e14e3d2b00a26873a5ff092a4e80f974d1b31
Hesham kamal <hesham@chefaa.com>
2019-06-02 15:00:12 +0200

    add comment

--- single_product.php
+++ single_product.php
@@ -2,7 +2,7 @@
 <?php require("header.php"); ?>
 <!-- End Calling Website Header -->
 
-<h1 style = "text-align:center" > Single Product Page </h1>
+<h1 style = "text-align:center" > Single Product Page </h1> <!-- Comment -->
 
 <!-- Start Calling Website Footer -->
 <?php require("footer.php"); ?>
