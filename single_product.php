bbfb4dd8107149a9da26144f0085fe56e0841657
Hesham kamal <hesham@chefaa.com>
2019-06-02 14:50:06 +0200

    create single product page

---
+++ single_product.php
@@ -0,0 +1,9 @@
+<!-- Start Calling Website Header -->
+<?php require("header.php"); ?>
+<!-- End Calling Website Header -->
+
+<h1> Single Product Page </h1>
+
+<!-- Start Calling Website Footer -->
+<?php require("footer.php"); ?>
+<!-- End Calling Website Footer -->
