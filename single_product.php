3e030524184373840090bc6d0921ae288a50b6fb
Hesham kamal <hesham@chefaa.com>
2019-06-02 15:10:10 +0200

    edit single product page and create custom css file for it

--- single_product.php
+++ single_product.php
@@ -2,7 +2,15 @@
 <?php require("header.php"); ?>
 <!-- End Calling Website Header -->
 
-<h1 style = "text-align:center" > Single Product Page </h1> <!-- Comment -->
+<!-- Start Calling Single Product Page CSS File -->
+<link rel = "stylesheet" href = "css/single_product.css" />
+<!-- End Calling Single Product Page CSS File -->
+
+<!-- Start Single Product Page -->
+
+<h1 style = "text-align:center" > Single Product Page </h1>
+
+<!-- End Single Product Page -->
 
 <!-- Start Calling Website Footer -->
 <?php require("footer.php"); ?>
