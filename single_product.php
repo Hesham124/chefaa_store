0e6434571fc37d516f50208cd67c225c01c67cc7
Hesham kamal <hesham@chefaa.com>
2019-06-09 10:41:37 +0200

    continue design of single product page

--- single_product.php
+++ single_product.php
@@ -8,7 +8,77 @@
 
 <!-- Start Single Product Page -->
 
-<h1 style = "text-align:center" > Single Product Page </h1>
+<section id = "single_product" class = "single_product" >
+     <div class = "container" >
+         <div class = "row" >
+              <div class = "col-lg-6" >
+                  <div class = "single_product_image" >
+                    <div class = "discount" >40%</div>
+                    <img src = "images/single_product.png" alt = "single_product_image" class = "img-responsive" style = "width:100% !important" />
+                  </div>
+              </div>
+              <div class = "col-lg-6" >
+                   <div class = "single_product_info" >
+                       <span class = "product_title" > Himalaya</span> 
+                       <p> كريم تفتيح الجسم من هيمالايا , 60 مل </p>
+                       <div class = "product_price" >
+                          <del> 50 جنيه  </del>
+                          <span> 250 جنيه </span>
+                          <span class = "price_saving" > وفرت 400 جنيه  </span>
+                       </div>
+                       <div class = "product_purchasing_details" >
+                          <span> كمية  </span>
+                          <ul>
+                             <li>
+                                <select>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                </select>
+                             </li>
+                             <li>
+                                <button> اضف للعربة </button>
+                             </li>
+                             <li>
+                                <i class="fa fa-heart-o fa-2x fa-fw" aria-hidden="true"></i>
+                             </li>
+                          </ul>
+                       </div>
+                   </div>
+              </div>
+         </div>
+     </div>
+</section>
+
+<section id = "single_product_details" class = "single_product_details" >
+    <div class = "empty" ></div>
+    <!-- Start Product Trademark -->
+    <div class = "trade_mark" >
+       <div class = "container" >
+           <div class = "row" >
+               <div class = "col-lg-12" >
+                 <div class = "trade_mark_content" >
+                 العلامة التجارية : <span> بيوبالاتس </span>
+                 </div>
+               </div>
+           </div>
+      </div>
+    </div> 
+    <!-- End Product Trademark -->
+    <!-- Start Product Type -->
+    <div class = "type" >
+       <div class = "container" >
+           <div class = "row" >
+               <div class = "col-lg-12" >
+                 <div class = "type_content" >
+                     النوع : <span> كريم تفتيح </span>
+                 </div>
+               </div>
+           </div>
+      </div>
+    </div> 
+    <!-- End Product Type -->
+</section>
 
 <!-- End Single Product Page -->
 
