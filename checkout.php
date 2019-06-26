1ffc14cf646f3cfb2b984a54eba12ea90d3a8a2d
Hesham kamal <hesham@chefaa.com>
2019-06-26 10:45:44 +0200

    edit

---
+++ checkout.php
@@ -0,0 +1,15 @@
+<!-- Start Calling Website Header -->
+<?php require("header.php"); ?>
+<!-- End Calling Website Header -->
+<link rel = 'stylesheet' href = 'css/checkout.css' /> <!-- Checkout CSS File -->
+
+<!-- Start Checkout Page Content -->
+
+<div> Checkout Page Content </div>
+
+<!-- End Checkout Page Content -->
+
+<!-- Start Calling Website Footer -->
+<?php require("footer.php"); ?>
+<!-- End Calling Website Footer --> 
+<script src = "js/checkout.js" ></script> <!-- Checkout JS File -->
