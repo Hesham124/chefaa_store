b9974c1db63441248766267f80f00c001cedc455
Hesham kamal <hesham@chefaa.com>
2019-07-03 15:49:57 +0200

    design address step

--- checkout.php
+++ checkout.php
@@ -8,6 +8,53 @@
 
 <!-- Start Checkout Page Content -->
 
+<!-- Start Modals -->
+
+<!-- Start Add New Location Modal -->
+
+<div class="modal fade" id="add_new_location" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"> اضافة عنوان </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <hr />
+      <div class="modal-body">
+        <div id = "new_location_infos" class = "new_location_infos" >
+            <!-- Start Client Name -->
+            <div class = "form-group" >
+                <label for = "client_name" > الاسم </label>
+                <input type = "text" name = "client_name" class = "form-control" id = "client_name" placeholder = "الاسم" />
+            </div>
+            <!-- End Client Name -->
+            <!-- Start Client Address -->
+            <div class = "form-group" >
+                <label for = "client_address" > العنوان </label>
+                <textarea name = "client_address" class = "form-control" id = "client_addresss" rows = "3" placeholder = "العنوان" ></textarea>
+            </div>
+            <!-- End Client Address -->
+            <!-- Start Client Phone Number -->
+            <div class = "form-group" >
+                <label for = "client_phone" > رقم الاتصال  </label>
+                <input type = "text" name = "client_phone" id = "client_phone" class = "form-control" placeholder = "رقم الاتصال" maxlength = "11" />
+            </div>
+            <!-- End Client Phone Number -->
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary"> اضافة عنوان </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- End Add New Location Modal -->
+
+<!-- End Modals -->
+
 <section id = "checkout" class = "checkout" >
     <div class = "container" >
         <div class = "row" >
@@ -32,6 +79,85 @@
         </div>
     </div>
 </section>
+
+<!-- Start Purchasing Form -->
+
+<section id = "purchase_form" class = "purchase_form" >
+      <!-- Start Purchase Form -->
+      <form action = "" method = "" enctype = "multipart/form-data" >
+         <!-- Start Form Steps -->
+         <ul id = "progressbar" >
+             <li class = "active" >
+                 <span> العنوان </span>
+             </li>
+             <li>
+                 <span> طرق الدفع </span>
+             </li>
+             <li>
+                 <span> تاكيد الطلب </span>
+             </li>
+         </ul>
+
+         <!-- Start Steps Content -->
+
+         <div class = "step_content" >
+             <div class = "container" >
+                 <div class = "row" >
+                     <div class = "col-lg-4" >
+                        <div class = "location_infos" >
+                            <!-- Start Location Icons -->
+                            <div class = "location_icons" >
+                                <div class = "location_icon" >
+                                    <span> <i class="fa fa-map-marker" aria-hidden="true"></i> </span>
+                                </div>
+                                <div class = "dots_icon" >
+                                    <span> <i class="fa fa-ellipsis-h" aria-hidden="true"></i> </span>
+                                </div>
+                            </div> 
+                            <!-- End Location Icons -->
+                            <!-- Start Location Info -->
+                            <div class = "location_info" >
+                                <h6> الاسم </h6>
+                                <p> محمد احمد عبد الحميد </p>
+                                <h6> العنوان </h6>
+                                <p>
+                                   شارع 151 مبنى 4 الدور التاسع شقة رقم 903 المعادى - القاهرة
+                                </p>
+                                <h6>  رقم الاتصال </h6>
+                                <p> 07775200 </p>
+                            </div>
+                            <!-- End Location Info -->
+                            <!-- Start Verified Location -->
+                            <div id = "verified_location" title = "verified" >
+                                <span>
+                                   <i class="fa fa-check" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <!-- End Verified Location -->
+                        </div>
+                     </div>
+                     <div class = "col-lg-4" >
+                        <div class = "add_location" >
+                            <a data-toggle="modal" data-target="#add_new_location" >
+                                <span>
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                                </span>
+                                <p> اضافة<br /> عنوان جديد </p>
+                            </a>
+                        </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+ 
+         <!-- End Steps Content -->
+
+         <!-- End Form Steps -->
+      </form>
+      <!-- End Purchase Form -->
+</section>
+
+<!-- End Purchasing Form -->
 
 <!-- End Checkout Page Content -->
 
