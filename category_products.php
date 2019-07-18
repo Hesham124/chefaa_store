0a896b45f1db096b51801f2f97cc5405ed85b929
Hesham kamal <hesham@chefaa.com>
2019-07-18 13:59:41 +0200

    start working with category_products page

---
+++ category_products.php
@@ -0,0 +1,21 @@
+<!-- Start Calling Website Header -->
+<?php require("header.php"); ?>
+<!-- End Calling Website Header -->
+
+<!-- Start Calling Category Products Css File -->
+<link rel = "stylesheet" href = "css/category_products.css" />
+<!-- End Calling Category Products Css File -->
+
+<!-- Start Category Products Page Content -->
+
+
+
+<!-- End Category Products Page Content -->
+
+<!-- Start Calling Website Footer -->
+<?php require("footer.php"); ?>
+<!-- End Calling Website Footer -->
+
+<!-- Start Calling Category Products JS File -->
+<script src = "js/category_products.js" ></script>
+<!-- End Calling Category Products JS File -->
---
+++ js/category_products.js
@@ -0,0 +1,11 @@
+/* Start JS [ JQuery ] Code */
+
+$(document).ready(function() {
+
+     "use strict";
+
+     
+
+});
+
+/* End JS [ JQuery ] Code */
