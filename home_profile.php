bcf3328cee0654567e7d2aface85495b1971ef9d
Hesham kamal <hesham@chefaa.com>
2019-07-22 10:52:05 +0200

    start design the home profile page

---
+++ home_profile.php
@@ -0,0 +1,21 @@
+<!-- Start Include Website Header -->
+<?php require("header.php"); ?>
+<!-- End Include Website Header -->
+
+<!-- Start Calling Home Profile CSS File -->
+<link rel = "stylesheet" href = "css/home_profile.css" />
+<!-- End Calling Home Profile CSS File -->
+
+<!-- Start Home Profile Page Content -->
+
+
+
+<!-- End Home Profile Page Content -->
+
+<!-- Start Calling Home Profile JS File -->
+<script src = "js/home_profile.js" ></script>
+<!-- End Calling Home Profile JS File -->
+
+<!-- Start Include Website Footer -->
+<?php require("footer.php"); ?>
+<!-- End Include Website Footer -->
